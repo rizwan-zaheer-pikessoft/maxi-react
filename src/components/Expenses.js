7aadc30cfa11af864d368ba8194366bf6bdac026
RizwanGhumman <rizwan.zaheer@pikessoft.com>
2023-08-26 00:47:06 +0500

    first assignment

---
+++ src/components/Expenses.js
@@ -0,0 +1,19 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+function Expenses(props){
+
+    return (
+        <div className="expenses">
+            <div>
+                <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
+            </div>
+            <div>
+                <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
+            </div>
+            <div>
+                <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
+            </div>
+        </div>
+    )
+}
+export default Expenses;
