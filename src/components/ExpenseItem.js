4b1183ce3ca1188b702b87255a46f2fd09b7cd6e
RizwanGhumman <rizwan.zaheer@pikessoft.com>
2023-08-25 14:24:16 +0500

    MAXI-45:expense component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <div>28 March 2023</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <p>$294.3</p>
+            </div>
+        </div>
+    )
+}
+export default ExpenseItem;
