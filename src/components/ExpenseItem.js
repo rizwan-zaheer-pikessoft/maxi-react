a75204c04050dd54fad57df30b3e33b0a2845a15
RizwanGhumman <rizwan.zaheer@pikessoft.com>
2023-08-25 14:34:52 +0500

    MAXI-46:expense component css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,11 @@
+import './ExpenseItem.css'
 function ExpenseItem(){
     return (
-        <div>
-            <div>28 March 2023</div>
-            <div>
+        <div className="expense-item">
+            <div>March 28th 2023</div>
+            <div className="expense-item__description">
                 <h2>Car Insurance</h2>
-                <p>$294.3</p>
+                <div className="expense-item__price">$294.3</div>
             </div>
         </div>
     )
