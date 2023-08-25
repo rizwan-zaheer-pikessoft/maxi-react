e8f9c1275cfefaea9ab9f05a57fd854499f3c408
RizwanGhumman <rizwan.zaheer@pikessoft.com>
2023-08-25 23:42:18 +0500

    MAXI-50:format date

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-        <ExpenseItem expense={expenses[0]}></ExpenseItem>
-        <ExpenseItem expense={expenses[1]}></ExpenseItem>
-        <ExpenseItem expense={expenses[2]}></ExpenseItem>
+        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
 
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,18 @@
 import './ExpenseItem.css'
 function ExpenseItem(props){
+    const month = new Date(props.date).toLocaleString('en-US',{month:'long'});
+    const day = new Date(props.date).toLocaleString('en-US',{day:'2-digit'});
+    const year = new Date(props.date).getFullYear();
     return (
         <div className="expense-item">
-            <div>{props.expense.date}</div>
+            <div>
+                <div>{day}</div>
+                <div>{month}</div>
+                <div>{year}</div>
+            </div>
             <div className="expense-item__description">
-                <h2>{props.expense.title}</h2>
-                <div className="expense-item__price">${props.expense.amount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     )
