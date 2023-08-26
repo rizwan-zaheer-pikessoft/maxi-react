5374fc32361b7cb35c3ca454eb61042ba29ee0dd
RizwanGhumman <rizwan.zaheer@pikessoft.com>
2023-08-27 00:16:56 +0500

    MAXI-72:add form

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,11 @@
+import ExpenseForm from "./ExpenseForm";
+import './NewExpense.css'
+
+export const NewExpense = () => {
+    return (
+        <div className="new-expense">
+            <ExpenseForm/>
+        </div>
+    )
+}
+export default NewExpense;
