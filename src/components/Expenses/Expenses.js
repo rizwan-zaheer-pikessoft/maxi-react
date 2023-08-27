2ae83740f0871012bf70ef565dd20ce3196d0e2f
RizwanGhumman <rizwan.zaheer@pikessoft.com>
2023-08-27 12:55:22 +0500

    MAXI-86:render listing

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,17 +12,9 @@ const Expenses = (props) => {
     }
     return (
         <div>
-            <ExpenseFilter onSelected = {filteredYear} onChangeYearFilter={onChangeYearFilter}/>
             <Card className="expenses">
-                <div>
-                    <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
-                </div>
-                <div>
-                    <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
-                </div>
-                <div>
-                    <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
-                </div>
+                <ExpenseFilter onSelected = {filteredYear} onChangeYearFilter={onChangeYearFilter}/>
+                {props.items.map(expenseItem=><ExpenseItem title = {expenseItem.title} amount={expenseItem.amount} date={expenseItem.date}/>)}
             </Card>
         </div>
     )
