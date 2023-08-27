97a70343b23fa3f2a2f43105eaa30950f3bf0798
RizwanGhumman <rizwan.zaheer@pikessoft.com>
2023-08-27 17:28:46 +0500

    MAXI-89:conditional content render

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,18 +6,25 @@ import '../ExpenseFilter/ExpenseFilter.css'
 import {useState} from "react";
 
 const Expenses = (props) => {
-    const [filteredYear,setFilteredYear] = useState('2020');
-    const onChangeYearFilter = (selectedYear)=>{
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const onChangeYearFilter = (selectedYear) => {
         setFilteredYear(selectedYear);
     }
-    const filteredItems = props.items.filter(expense =>{
-        return expense.date.getFullYear().toString()===filteredYear;
+    const filteredItems = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
     })
+    let expenseContent = <p>No expense found.</p>
+    if (filteredItems.length > 0) {
+        expenseContent = filteredItems.map(expenseItem => <ExpenseItem
+            key={expenseItem.id} title={expenseItem.title}
+            amount={expenseItem.amount}
+            date={expenseItem.date}/>)
+    }
     return (
         <div>
             <Card className="expenses">
-                <ExpenseFilter onSelected = {filteredYear} onChangeYearFilter={onChangeYearFilter}/>
-                {filteredItems.map(expenseItem=><ExpenseItem key={expenseItem.id} title = {expenseItem.title} amount={expenseItem.amount} date={expenseItem.date}/>)}
+                <ExpenseFilter onSelected={filteredYear} onChangeYearFilter={onChangeYearFilter}/>
+                {expenseContent}
             </Card>
         </div>
     )
